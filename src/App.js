82a8843a6979e663a817bb80052f793931d967f4
Dilip Konigiri <dilipkonigiri@gmail.com>
2020-07-19 22:50:21 -0700

    Setup until navbar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <>
-    <Home/>
-    <Rooms/>
-    <SingleRoom/>
-    <Error/>
+    <Navbar/>
+    <Switch>
+    <Route exact path="/"><Home/></Route>
+    <Route exact path="/rooms/"><Rooms/></Route>
+    <Route exact path="/rooms/:slug"><SingleRoom/></Route>
+    <Route><Error/></Route>
+    </Switch>
     </>
   );
 }
