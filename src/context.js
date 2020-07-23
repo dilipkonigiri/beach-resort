5eccaefc01b9323f465641e0ea88b187f5d0785d
Dilip Konigiri <dilipkonigiri@gmail.com>
2020-07-23 00:18:40 -0700

    Completed featured rooms basic setup with component

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import items from './data'
 
 const RoomContext = React.createContext() // contains two components (Provider, Consumer)
 // <RoomContext.Provider value={'hello}
@@ -8,10 +9,36 @@ class RoomProvider extends Component {
     constructor() {
         super()
         this.state = {
-            greeting: "hello",
-            name: "john"
+            rooms: [],
+            sortedRooms: [],
+            featuredRooms: [],
+            loading: true
         }
     }
+
+    componentDidMount() {
+        let rooms = this.formatData(items)
+        // console.log(rooms);
+        let featuredRooms = rooms.filter(room => room.featured === true)
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        })
+        
+    }
+
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id
+            let images = item.fields.images.map(image => image.fields.file.url)
+            let room = { ...item.fields, images, id }
+            return room
+        })
+        return tempItems
+    }
+
     render() {
         return (
             <RoomContext.Provider value={{ ...this.state }}>
