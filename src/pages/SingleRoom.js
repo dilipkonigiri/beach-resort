c1dd44dfd22c6d8a358a1562def7c9bc606a6edb
Dilip Konigiri <dilipkonigiri@gmail.com>
2020-07-27 00:30:13 -0700

    Updated

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
+    constructor(props) {
+        super(props)
+        console.log(this.props)
+        // this.state = {
+        //     slug: this.props.match.params.slug
+        // }
+    }
+
+    // componentDidMount() {
+    // }
+    
+
     render() {
         return (
             <div>
