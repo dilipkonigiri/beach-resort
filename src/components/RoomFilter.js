287b3e25ea9b5c124926c9883482d0fa5459fdc7
Dilip Konigiri <dilipkonigiri@gmail.com>
2020-07-31 02:17:13 -0700

    Updated

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useContext } from 'react'
+import { RoomContext } from '../context'
+import Title from '../components/Title'
 
 export default function RoomFilter() {
+    const context = useContext(RoomContext)
+    console.log(context);
+
+    const {
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+    } = context
+    
     return (
-        <div>
-            hello from Rooms Filter
-        </div>
+        <section className="filter-container">
+            <Title title="search rooms"/>
+        </section>
     )
 }
